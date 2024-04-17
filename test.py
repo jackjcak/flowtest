cd64ddd53b3c65ede8fd6fdebd6963d960ba21d7
moon <moon@163.com>
2024-04-17 20:00:59 +0800

    first

---
+++ test.py
@@ -0,0 +1,7 @@
+#
+# import gdown
+# url="https://drive.google.com/uc?id=1g8j-hTT-51IG1UFwP0xTGhLdgIUCW5e5&export=download"
+# gdown.download(url, quiet=False, proxy="192.168.123.53:8888")
+
+import pip
+print(pip)
